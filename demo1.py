0032b7f9362b1ff20aafb785fb98af4242506cee
sunjinshuai <s_mike@163.com>
2017-07-24 22:47:36 +0800

    python

---
+++ demo1.py
@@ -0,0 +1,12 @@
+
+# 题目： 获取 100 以内的质数。
+num = [];
+i = 2
+for i in range(2, 100):
+   j = 2
+   for j in range(2, i):
+      if(i % j == 0):
+         break
+   else:
+      num.append(i)
+print(num)
