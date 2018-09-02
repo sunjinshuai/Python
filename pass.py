ccd2aa848d1a57e1751f886844fa49e3f216b439
sunjinshuai <s_mike@163.com>
2018-09-02 22:02:32 +0800

    Python pass 语句

---
+++ pass.py
@@ -0,0 +1,12 @@
+# Python pass是空语句，是为了保持程序结构的完整性。
+
+# pass 不做任何事情，一般用做占位语句。
+
+# 输出 Python 的每个字母
+for letter in 'Python':
+   if letter == 'h':
+      pass
+      print '这是 pass 块'
+   print '当前字母 :', letter
+
+print "Good bye!"
